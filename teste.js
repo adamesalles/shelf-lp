bbb180cfc3f731b72fb4dc732b82f99bf83c5b22
Ana Carolina Erthal <81707790+anacarolerthal@users.noreply.github.com>
2021-09-25 18:44:21 -0300

    Pag livros completa, falta parte visual e banner

--- teste.js
+++ teste.js
@@ -1,13 +1,77 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let items1 = document.querySelectorAll('.c-1 .ci-1')
 
-items.forEach((el) => {
-    const minPerSlide = 5
+items1.forEach((el) => {
+    const minPerSlide = 6
     let next = el.nextElementSibling
     for (var i=1; i<minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
-        	next = items[0]
-      	}
+            next = items1[0]
+          }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let items2 = document.querySelectorAll('.c-2 .ci-2')
+
+items2.forEach((el) => {
+    const minPerSlide = 6
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items2[0]
+          }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let items3 = document.querySelectorAll('.c-3 .ci-3')
+
+items3.forEach((el) => {
+    const minPerSlide = 6
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items3[0]
+          }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let items4 = document.querySelectorAll('.c-4 .ci-4')
+
+items4.forEach((el) => {
+    const minPerSlide = 6
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items4[0]
+          }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let items5 = document.querySelectorAll('.c-5 .ci-5')
+
+items5.forEach((el) => {
+    const minPerSlide = 6
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items5[0]
+          }
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
